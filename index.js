50b647139b470f7af7a8088741c7c1c4098d1d68
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2022-06-21 10:21:29 +0200

    Bonus exercise

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 // console.log(stringified)
 
 // 03 - Round
-// const num = 1.5
+// const num = 1.5 
 // const rounded = Math.round(num)
 // console.log(rounded)
 
@@ -23,15 +23,28 @@
 // console.log (test % bis)
 
 // 05 - Reste division euclidienne
-const number = 691
-console.log (number % 1)
-console.log (number % 2)
-console.log (number % 3)
-console.log (number % 4)
-console.log (number % 5)
+// const number = 691
+// console.log (number % 1)
+// console.log (number % 2)
+// console.log (number % 3)
+// console.log (number % 4)
+// console.log (number % 5)
 
-console.log ("1 et 3 divisent complètement 999")
-console.log ("1 et 5 divisent complètement 25")
-console.log ("1, 2, 4 et 5 divisent complètement 190280")
-console.log ("1 divise complètement 47")
-console.log ("1 divise complètement 691")
+// console.log ("1 et 3 divisent complètement 999")
+// console.log ("1 et 5 divisent complètement 25")
+// console.log ("1, 2, 4 et 5 divisent complètement 190280")
+// console.log ("1 divise complètement 47")
+// console.log ("1 divise complètement 691")
+
+// Bonus
+let numbers = [999, 25, 190280, 47, 691]
+
+for (let number in numbers) {
+
+    for (i=1; i <=5; i += 1){
+
+        if (numbers[number] % i == 0){
+            console.log(`${numbers[number]} est divisible complètement par ${i}`)
+        }
+    }
+}
